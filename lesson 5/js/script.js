9c91a550cc47a37df554c3ba65c7a2f9b3872501
RAIL <traclandfieldd@gmail.com>
2020-11-12 23:48:07 +0300

    пятый комит

--- lesson 5/js/script.js
+++ lesson 5/js/script.js
@@ -2,15 +2,32 @@
 let newLi = document.createElement('li');
 let newUl = document.querySelector('nav>ul.menu');
 let menu = document.querySelectorAll('.menu-item');
+let adv = document.querySelector('.adv');
+let title = document.querySelector('.title');
+let prompT = document.querySelector('.prompt');
+let temp;
+
    
 
 newLi.classList.add('menu-item');
 newLi.innerText = 'Пятый пункт';
 newUl.appendChild(newLi);
+adv.remove();
+document.body.style.backgroundImage = "url(img/true.jpg)";
+title.innerHTML = 'Мы продаем только подлинную технику Apple';
 
-menu.forEach(function(item, i, elements) {
+/*let question = prompt("Ваше отношение к технике Apple?", '');  
+prompT.innerHTML = question;*/
 
-});
+temp = menu[1];
+menu[1] = menu[2];
+menu[2] = temp; 
+
+
+
+//menu.forEach(function(item, i, elements) {
+
+//});
 
 
 
