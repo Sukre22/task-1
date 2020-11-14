0234bbee4b088ce183ad725cb3fdf0ac99a90c73
RAIL <traclandfieldd@gmail.com>
2020-11-14 13:02:00 +0300

    commit lesson 7

---
+++ lesson 6/src/js/main.js
@@ -0,0 +1,27 @@
+let startButton = document.getElementById('start');
+
+let budgetValue = document.getElementsByClassName('budget-value');
+let DaybudgetValue = document.getElementsByClassName('daybudget-value');
+let levelValue = document.getElementsByClassName('level-value');
+let expensesValue = document.getElementsByClassName('expenses-value');
+let OptionalexpensesValue = document.getElementsByClassName('optionalexpenses-value');
+let incomeValue = document.getElementsByClassName('income-value');
+let monthSavings = document.getElementsByClassName('monthsavings-value');
+let yearSavings = document.getElementsByClassName('yearsavins-value');
+
+let inputExpenses = document.getElementsByClassName('expenses-item');
+let inputOptionalexpenses = document.querySelectorAll('.optionalexpenses-item');
+let expensesItemBtn = document.getElementsByClassName('expenses-item-btn');
+let OptionalexpensesBtn = document.getElementsByClassName('optionalexpenses-btn');
+let CountbudgetBtn = document.getElementsByClassName('count-budget-btn');
+
+let chooseIncome = document.querySelector('.choose-income');
+let checkSavings = document.querySelector('.checksavings');
+let chooseSum = document.querySelector('.choose-sum');
+let choosePersent = document.querySelector('.choose-persent');
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
+
+
+
