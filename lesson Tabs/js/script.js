a29925a46acea5c53e17713dd8052578b79964d2
RAIL <traclandfieldd@gmail.com>
2020-11-22 19:38:20 +0300

    Timer

--- lesson Tabs/js/script.js
+++ lesson Tabs/js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info--header'),
+        info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
@@ -33,4 +33,63 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+   // Timer
+
+   let deadLine = '2020-11-23';
+
+   function getTimeRemaining(endtime) {
+       let t = Date.parse(endtime) - Date.parse(new Date()),
+           seconds = Math.floor((t/1000) % 60),
+           minutes = Math.floor((t/1000/60) % 60),
+           hours = Math.floor((t/(1000*60*60)));
+
+           return {
+               'total': t,
+               'hours': hours,
+               'minutes': minutes,
+               'seconds': seconds
+
+           };
+           
+   }
+
+   function setClock(id, endtime) {
+       let timer = document.getElementById(id),
+           hours = timer.querySelector('.hours'),
+           minutes = timer.querySelector('.minutes'),
+           seconds = timer.querySelector('.seconds'),
+           timeInterval = setInterval(updateClock, 1000);
+           
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if  (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }  
+
+       
+     
+
+   }
+
+   
+   setClock('timer', deadLine);
+
+   
+
+function addZero(num) {
+    if (num <= 9) {
+     return '0'+num;
+    }
+     
+     else {
+         return  num;
+     } 
+    } 
+
 });
