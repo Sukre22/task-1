89101b26c08b4c1a7f4db49879fe0557bcf6abaf
RAIL <traclandfieldd@gmail.com>
2021-01-13 23:20:40 +0300

    lesson jquery

--- Promise/js/script.js
+++ Promise/js/script.js
@@ -1,22 +1,39 @@
-
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
 
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
+
+
+    let promise = new Promise(function(resolve, reject){
+    inputRub.addEventListener('input', () => {
+
+        
+          
+         let request = new XMLHttpRequest();
+     
+         request.open('GET', 'js/current.json');
+         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+         request.send();
+         
+         request.addEventListener('readystatechange', function() {
+             if (request.readyState === 4 && request.status == 200) {
+                 resolve();            
+             } else {
+                 reject();
+             }
+         });
+     
+                
+     
+     });
+     return promise;       
+}); 
+
+
+promise
+     .then(() => {
+        let data = JSON.parse(request.response);
+        inputUsd.value = inputRub.value / data.usd; 
+     })
+     .catch(() => inputUsd.value = "Что-то пошло не так!"); 
     
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
-        }
-    });
-
-});
---
+++ lesson jQuery/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#1').on('click', function() {
+        $('.modal').css('display', 'block');
+    });
+    $('#1').on('click', function() {
+        $('.overlay').css('display', 'block');
+    });
+    $('.close').on('click', function() {
+        $('.modal').css('display', 'none');
+    });
+    $('.close').on('click', function() {
+        $('.overlay').css('display', 'none');
+    });
+
+    
+               
+});
