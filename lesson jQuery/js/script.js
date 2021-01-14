4da89492980e3c5378e3e75d7f0af7c961f23349
RAIL <traclandfieldd@gmail.com>
2021-01-14 22:42:47 +0300

    lesson JQuery

--- lesson jQuery/js/script.js
+++ lesson jQuery/js/script.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
     $('#1').on('click', function() {
-        $('.modal').css('display', 'block');
+        $('.modal').fadeIn('slow', function() {
+            $('.modal').css('display', 'block');
+        });
+        
+        
     });
+
+    $('.close').on('click', function() {
+        $('.modal').fadeToggle('slow', function() {
+            $('.modal').css('display', 'none');
+        });
+        
+
+    });
+      
     $('#1').on('click', function() {
         $('.overlay').css('display', 'block');
     });
-    $('.close').on('click', function() {
-        $('.modal').css('display', 'none');
-    });
+  
+        
+
+    
     $('.close').on('click', function() {
         $('.overlay').css('display', 'none');
     });
